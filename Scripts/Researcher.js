1b349ac607576915e857e0a93080e700c23fbeb7
Marlven <mbhunushava@gmail.com>
2022-10-21 11:58:06 +0200

    Researcher Script Fixed

--- Scripts/Researcher.js
+++ Scripts/Researcher.js
@@ -1,17 +1,20 @@
-const form = document.querySelector("Form"),
-fileInput = form.querySelector(".File-Input"),
-progressArea = document.querySelector(".Progress-Area"),
-uploadedArea = document.querySelector(".Uploaded-Area");
+const form = document.querySelector('form'),
+fileInput = form.querySelector('.File-Input'),
+progressArea = document.querySelector('.Progress-Area'),
+uploadedArea = document.querySelector('.Uploaded-Area');
 
-form.addEventListener("click", ()=>{
+form.addEventListener('click', ()=>{
     fileInput.click();
 })
 
 fileInput.onchange = ({target}) =>{
-    let File = target.Files[0]; //Get file. [0] means if user has multiple files selected, then get first file
+    
+    let File = target.files[0]; //Get file. [0] means if user has multiple files selected, then get first file
+    
     if(File){ //If file is selected 
-        let FileName = File.Name; //Get selected file naem
-        if(FileName.lenght >= 12){ //If FileName length is >= 12, the split name and add
+            let FileName = File.Name; //Get selected file naem
+    
+        if(FileName.length >= 12){ //If FileName length is >= 12, the split name and add
             let splitName = FileName.split('.');
             FileName = splitName[0].substring(0, 12) + "... ." + splitName[1];
         }
@@ -22,31 +25,31 @@ fileInput.onchange = ({target}) =>{
 function uploadFile(Name){
 
     let xs = new XMLHttpRequest(); //Create AJAX
-    xs.open("POST", "php/upload.php"); //Send post request to specified URL/File
-    xs.upload.addEventListener("progress", ({loaded, total}) =>{
+    xs.open('POST', 'php/upload.php'); //Send post request to specified URL/File
+    xs.upload.addEventListener('progress', ({loaded, total}) =>{
     
         let fileLoaded = Math.floor((loaded / total) * 100); //Get percentage of loaded file size 
     
         let fileTotal = Math.floor(total / 1000); //Get fle size in KB from Bytes
     
         let fileSize; //If file size is <1024 then add only KB else covert size in KB to MB
-        (fileTotal < 1024) ? fileSize = fileTotal + "KB" : fileSize = (loaded / 1024*1024).toFixed(2) + "MB";
+        (fileTotal < 1024) ? fileSize = fileTotal + 'KB' : fileSize = (loaded / 1024*1024).toFixed(2) + 'MB';
 
         let progressHTML = '<li class="Row"> <i class="fas fa-file-alt"></i> <div class="Content"> <div class="Details"> <span class="Name">${Name} - Uploading</span> <span class="Percent">${fileLoaded}%</span> </div><div class="Progress-Bar"> <div class="Progress" style="width: ${fileLoaded"></div> </div></div></li>';
 
-        uploadedArea.classList.add("onprogress");
+        uploadedArea.classList.add('onprogress');
 
     progressArea.innerHTML = progressHTML;
     
     if(loaded == total){
     
-        progressArea.innerHTML = "";
+        progressArea.innerHTML = '';
         
         let uploadedHTML = '<li class="Row"> <div class="Content"> <i class="fas fa-file-alt"></i> <div class="Details"> <span class="Name">${Name} - Uploaded</span> <span class="Size">${fileTotal}</span> </div> </div> <i class="fas fa-check"></i></li>';
 
-        uploadedArea.classList.remove("onprogress");
+        uploadedArea.classList.remove('onprogress');
 
-        uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML); //Remove the line if you deleted it
+        uploadedArea.insertAdjacentHTML('afterbegin', uploadedHTML); //Remove the line if you deleted it
     }
     });
 
--- Scripts/Sign-Up.js
+++ Scripts/Sign-Up.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", {} => {
-    const CreateAccount = document.querySelector(#CreateAccount);
+const form  = document.getElementById('form');
+const firstName  = document.getElementById('FN');
+const lastName = document.getElementById('LN');
+const email = document.getElementById('E');
+const organisation  = document.getElementById('Org');
+const position = document.getElementById('Pos');
+const password = document.getElementById('Pass');
+const passwordRe = document.getElementById('RePass');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
 });
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('')
+}
+
+
