4d2b91bca72e76294d45be7135679f724b9ac1ad
Marlven <mbhunushava@gmail.com>
2022-10-21 10:35:09 +0200

    Script made

--- Scripts/Website.js
+++ Scripts/Website.js
@@ -7,10 +7,17 @@ function closeMenu(){
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyfbcJvX_c_wx3-xKPhvOAUpu_d2xW4BqZbYzHhB352rIgTCLlANYNCikowGHAkGcppqw/exec'
   const form = document.forms['submit-to-google-sheet']
+  const message = document.getElementById("Message");
 
   form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
+      .then(response =>{
+        message.innerHTML = "Message sent sucessfully"
+        setTimeout(function(){
+            message.innerHTML = ""
+        }, 5000)
+        form.reset()
+      })
       .catch(error => console.error('Error!', error.message))
   })
